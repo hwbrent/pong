596d9bd2674c168e0b9911e727a606ff0547ade2
mustcat <hwbrent@icloud.com>
2023-01-14 11:52:32 +0000

    Implement Paddle up/down movement

--- src/Assets/Prefabs/Paddle/Paddle.cs
+++ src/Assets/Prefabs/Paddle/Paddle.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Camera camera;
+    private BoxCollider2D box;
+
+    // Used when moving the paddle up/down
+    private float movementFactor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.FindObjectOfType<Camera>();
+        box = this.GetComponent<BoxCollider2D>();
+        movementFactor = 0.0625f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.KeysAreDown()) this.ProcessKeyPress();
+    }
+
+    /// <summary>
+    /// Indicates whether exactly one of the up/down keys are currently pressed.
+    /// </summary>
+    bool KeysAreDown() {
+        return Input.GetKey(this.upKey) ^ Input.GetKey(this.downKey);
+    }
+
+    /// <summary>
+    /// Modifies `this.transform` in order to move the paddle up/down.
+    /// </summary>
+    void ProcessKeyPress() {
+        Vector3 maxPosInScreen = camera.WorldToScreenPoint(box.bounds.max);
+        Vector3 minPosInScreen = camera.WorldToScreenPoint(box.bounds.min);
+
+        // If the key for moving the paddle up is pressed, and the top of
+        // the paddle is below the top of the screen.
+        if (Input.GetKey(this.upKey) && maxPosInScreen.y < camera.pixelHeight)
+        {
+            this.transform.Translate(Vector3.up * this.movementFactor);
+        }
+
+        // If the key for moving the paddle down is pressed, and the top of
+        // the paddle is above the bottom of the screen.
+        else if (Input.GetKey(this.downKey) && minPosInScreen.y > 0)
+        {
+            this.transform.Translate(Vector3.down * this.movementFactor);
+        }
     }
 }
